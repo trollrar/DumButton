b3deb4c908cc1020ce0089aac805972bc8d1f432
farescalms <fares@calms.eu>
2020-08-22 00:41:05 +0200

    Add library and permissions

--- app/src/main/java/com/farikamo/dumbutton/MainActivity.kt
+++ app/src/main/java/com/farikamo/dumbutton/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.farikamo.dumbutton
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.ConnectionsClient
+import com.google.android.gms.nearby.connection.Strategy
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var connectionsClient: ConnectionsClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        connectionsClient = Nearby.getConnectionsClient(this)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onStart() {
+        super.onStart()
+        if (!hasPermissions(this, *REQUIRED_PERMISSIONS)) {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS)
+        }
+    }
+
+    private fun hasPermissions(context: Context, vararg permissions: String): Boolean {
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    companion object {
+        private const val REQUEST_CODE_REQUIRED_PERMISSIONS = 1
+        private val STRATEGY = Strategy.P2P_STAR
+        private val REQUIRED_PERMISSIONS = arrayOf(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
     }
 }
