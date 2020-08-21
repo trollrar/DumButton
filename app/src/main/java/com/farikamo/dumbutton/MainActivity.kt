0f06590ca396e5f4bcdf58abcfb6d9241e989bb8
farescalms <fares@calms.eu>
2020-08-22 01:57:51 +0200

    Add connections and layout

--- app/src/main/java/com/farikamo/dumbutton/MainActivity.kt
+++ app/src/main/java/com/farikamo/dumbutton/MainActivity.kt
@@ -6,13 +6,22 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.google.android.gms.nearby.Nearby
-import com.google.android.gms.nearby.connection.ConnectionsClient
-import com.google.android.gms.nearby.connection.Strategy
+import com.google.android.gms.nearby.connection.*
+import com.google.android.gms.nearby.connection.DiscoveryOptions
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.text.Charsets.UTF_8
+
 
 class MainActivity : AppCompatActivity() {
+
+
 
     private lateinit var connectionsClient: ConnectionsClient
 
@@ -38,6 +47,122 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    private val payloadCallback = object : PayloadCallback() {
+        override fun onPayloadReceived(endpointId: String, payload: Payload) {
+            // This always gets the full data of the payload. Will be null if it's not a BYTES
+            // payload. You can check the payload type with payload.getType().
+            setStatusText("received payload " + String(payload.asBytes()!!, UTF_8))
+        }
+
+        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
+            // Bytes payloads are sent as a single chunk, so you'll receive a SUCCESS update immediately
+            // after the call to onPayloadReceived().
+        }
+    }
+
+    private val endpointIds: MutableList<String> = mutableListOf()
+    private val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
+        override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
+            // Automatically accept the connection on both sides.
+            Nearby.getConnectionsClient(this@MainActivity).acceptConnection(endpointId, payloadCallback)
+            endpointIds.add(endpointId);
+            setStatusText("onConnectionInitiated ${connectionInfo.endpointName}")
+        }
+
+        override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+            when (result.status.statusCode) {
+                ConnectionsStatusCodes.STATUS_OK -> {
+                    setStatusText("onConnectionResult ${result.status}")
+                }
+                ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {
+                }
+                ConnectionsStatusCodes.STATUS_ERROR -> {
+                }
+            }// We're connected! Can now start sending and receiving data.
+            // The connection was rejected by one or both sides.
+            // The connection broke before it was able to be accepted.
+            // Unknown status code
+        }
+
+        override fun onDisconnected(endpointId: String) {
+            // We've been disconnected from this endpoint. No more data can be
+            // sent or received.
+        }
+    }
+
+    private val endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
+        override fun onEndpointFound(endpointId: String, info: DiscoveredEndpointInfo) {
+            // An endpoint was found. We request a connection to it.
+            Nearby.getConnectionsClient(this@MainActivity)
+                .requestConnection(getUserNickname(), endpointId, connectionLifecycleCallback)
+                .addOnSuccessListener { unused: Void ->
+                    // We successfully requested a connection. Now both sides
+                    // must accept before the connection is established.
+                }
+                .addOnFailureListener { e: Exception ->
+                    // Nearby Connections failed to request the connection.
+                }
+        }
+
+        override fun onEndpointLost(endpointId: String) {
+            // A previously discovered endpoint has gone away.
+        }
+    }
+
+    private fun startAdvertising() {
+        val advertisingOptions = AdvertisingOptions.Builder().setStrategy(STRATEGY).build()
+        Nearby.getConnectionsClient(this)
+            .startAdvertising(
+                getUserNickname(), packageName + getRoomName(), connectionLifecycleCallback, advertisingOptions
+            )
+            .addOnSuccessListener { unused: Void ->
+                // We're advertising!
+            }
+            .addOnFailureListener { e: Exception ->
+                // We were unable to start advertising.
+            }
+    }
+
+    private fun startDiscovery() {
+        val discoveryOptions = DiscoveryOptions.Builder().setStrategy(STRATEGY).build()
+        Nearby.getConnectionsClient(this)
+            .startDiscovery(packageName + getRoomName(), endpointDiscoveryCallback, discoveryOptions)
+            .addOnSuccessListener { unused: Void ->
+                // We're discovering!
+            }
+            .addOnFailureListener { e: Exception ->
+                // We're unable to start discovering.
+            }
+    }
+
+    private fun getUserNickname(): String {
+        return txt_nickname.toString()
+    }
+
+    private fun getRoomName(): String {
+        return txt_room_name.toString()
+    }
+
+    /** Finds an opponent to play the game with using Nearby Connections.  */
+    fun findOpponent(view: View) {
+        startAdvertising()
+        startDiscovery()
+        setStatusText("ZDAJ ISCEM")
+        btn_advrt_discover.isEnabled = false
+    }
+
+    fun ready(view: View) {
+        endpointIds.forEach { opponentEndpointId ->
+            Nearby.getConnectionsClient(this).sendPayload(
+                opponentEndpointId, Payload.fromBytes("ready".toByteArray(UTF_8))
+            )
+        }
+    }
+
+    fun setStatusText(status: String) {
+        txt_status.text = status
     }
 
     companion object {
